f6d3d454c449c82eeeb6b867a4b3b4452b41773c
Deep5317 <deep.adak90@gmail.com>
2024-07-13 11:24:43 +0530

    conversation sidebar setup

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,17 +1,22 @@
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput"
 import Messages from "./Messages"
 import { TiMessages } from "react-icons/ti";
 
 
 const MessageContainer = () => {
-    const nochatselected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-        {nochatselected ? <NoChatSelected/> : (
+        {!selectedConversation ? <NoChatSelected/> : (
             <>
             {/* Header */}
             <div className='bg-slate-500 px-4 py-2 mb-2'>
-                <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>Rudra bhai</span>
+                <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
             </div>
 
             <Messages />
