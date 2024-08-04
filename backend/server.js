97b25d3bd8ec12d59142f720f22af1d062414450
Deep5317 <deep.adak90@gmail.com>
2024-08-04 11:55:01 +0530

    socketio complete

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from './routes/user.routes.js'
 
 import connectToMongoDB from "./db/connectTomongoDb.js";
+import { app, server } from "./socket/socket.js"
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -23,7 +24,7 @@ app.use("/api/users",userRoutes);
 app.get("/",(req,res) => {
     res.send("Hello world!!");
 }); 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`server running on port ${PORT}`)
 });
