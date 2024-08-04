f2e3b49f685a8fdd27839748f97fb220de3c83aa
Deep5317 <deep.adak90@gmail.com>
2024-08-04 12:24:01 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -11,7 +12,7 @@ import { app, server } from "./socket/socket.js"
 
 // const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname=path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -21,9 +22,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.get("/",(req,res) => {
-    res.send("Hello world!!");
-}); 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+// app.get("/",(req,res) => {
+//     res.send("Hello world!!");
+// }); 
 server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`server running on port ${PORT}`)
